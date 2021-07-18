eee3f26e676f3592b8869da616552fc39d04bfff
Wutthisak Jongrahong <wutthisak.j@hotmail.com>
2021-07-18 17:18:36 +0700

    add merge and mergeSort

--- main.cpp
+++ main.cpp
@@ -112,6 +112,8 @@ struct Deck {
     void swapCards (int card1, int card2);
     bool shuffleDeck ();
     bool sortDeck ();
+    Deck subdeck (int low, int high) const;
+    Deck mergeSort() const;
 };
 
 Deck::Deck (int size) {
@@ -162,26 +164,6 @@ int newRandom (int min, int max) {
     return distr(gen);
 }
 
-int findBisect (const Card& card, const apvector<Card>& deck, int low, int high) {
-
-    cout << low << ", " << high << endl;
-
-    if (high < low) return -1;
-
-    int mid = (high + low) / 2;
-
-    // if we found the card, return its index
-    if (equals(deck[mid], card)) return mid;
-
-    // otherwise, compare the card to the middle card
-    if (deck[mid].isGreater (card)) {
-        // search the first half of the deck
-        return findBisect(card, deck, low, mid-1);
-    } else {
-        //search the second half of the deck
-        return findBisect(card, deck, mid+1, high);
-    }
-}
 void Deck::swapCards (int card1, int card2) {
     Card temp = Card();
     temp.suit = cards[card2].suit;
@@ -225,42 +207,121 @@ bool Deck::sortDeck() {
     return false;
 }
 
+Deck Deck::subdeck (int low, int high) const {
+    Deck sub (high-low+1);
+
+    for (int i = 0; i<sub.cards.length(); i++) {
+        sub.cards[i] = cards[low+i];
+    }
+    return sub;
+}
+
+int findBisect (const Card& card, const apvector<Card>& deck, int low, int high) {
+
+   // cout << low << ", " << high << endl;
+
+    if (high < low) return -1;
+
+    int mid = (high + low) / 2;
+
+    // if we found the card, return its index
+    if (equals(deck[mid], card)) return mid;
+
+    // otherwise, compare the card to the middle card
+    if (deck[mid].isGreater (card)) {
+        // search the first half of the deck
+        return findBisect(card, deck, low, mid-1);
+    } else {
+        //search the second half of the deck
+        return findBisect(card, deck, mid+1, high);
+    }
+}
+
+int findBisect (const Card& card, Deck& deck) {
+    int deckSize = deck.cards.length();
+
+    int mid = deckSize / 2;
+
+    if (equals(deck.cards[mid], card)) return mid;
+
+    if (deck.cards[mid].isGreater(card)) {
+        Deck sub = deck.subdeck(0, mid-1);
+        return findBisect(card, sub);
+    } else {
+        Deck sub = deck.subdeck(mid+1, deckSize-1);
+        return findBisect(card, sub);
+    }
+}
+
+Deck merge (const Deck& d1, const Deck& d2) {
+
+    int d1Length = d1.cards.length();
+    int d2Length = d2.cards.length();
+
+    // create a new deck big enough for all the cards
+    Deck result (d1Length + d2Length);
+
+    // use index i to keep track of where we are in the first deck
+    // and index j for the second deck
+    int i = 0, j = 0;
+
+    // the index k traverses the result deck
+    int resultLength = result.cards.length();
+    for (int k=0; k<resultLength; k++) {
+        // if d1 is empty, d2 wins; if d2 is empty, d1 wins
+        if ((i >= d1Length) and (j< d2Length))  {
+            result.cards[k].suit = d2.cards[j].suit;
+            result.cards[k].rank = d2.cards[j].rank;
+        } else if ((i < d1Length) and (j >= d2Length))  {
+            result.cards[k].suit = d1.cards[i].suit;
+            result.cards[k].rank = d1.cards[i].rank;
+        } else
+        // otherwise, compare the two cards
+        {
+            if (d1.cards[i].isGreater(d2.cards[j])) {
+                result.cards[k].suit = d2.cards[j].suit;
+                result.cards[k].rank = d2.cards[j].rank;
+                j++;
+            } else {
+                result.cards[k].suit = d1.cards[i].suit;
+                result.cards[k].rank = d1.cards[i].rank;
+                i++;
+            }
+         }
+        }
+    return result;
+}
+
+Deck Deck::mergeSort() const {
+    Deck result = Deck ();
+    int cardsLength = this->cards.length();
+    int mid = (cardsLength - 1) / 2;
+
+    Deck d1 = this->subdeck(0, mid);
+    Deck d2 = this->subdeck(mid + 1, cardsLength - 1);
+
+    d1.sortDeck();
+    cout << "--- D1 deck ----- " << endl;
+    d1.print();
+    cout << "--- D2 deck ----- " << endl;
+    d2.sortDeck();
+    d2.print();
+
+
+    return result = merge(d1, d2);;
+}
+
 int main()
 {
-    //Card TheJoker = Card ();
-    //Card TheJoker(void);
-    //Card JackOfDiamons (DIAMONDS, JACK);
-    //Card FifteenOfDiamons (JOKER, NORANK);
-    //apvector<Card> deck = buildDeck ();
-    //printDeck (deck);
-    //TheJoker.print();
-    //int index = find (JackOfDiamons, deck);
-    //cout << "I found the card at index = " << index <<  endl;
-
-    // create a deck (52 cards)
     Deck deck = Deck ();
+    Card JackOfDiamons (DIAMONDS, JACK);
+    Card KingOfSpades (SPADES, KING);
 
-    //deck.print();
-    if (deck.shuffleDeck()) {
-        deck.print();
-        //cout << "<< Shuffled and you've got ... >>" << endl;
-        //for (int i=0; i<2; i++) deck.cards[newRandom(0, 51)].print();
-        //cout << "The lowest card is located at : " << findLowestCard(deck, 0) << endl;
-        //deck.cards[findLowestCard(deck, 0)].print();
-    } else cout << "it's an empty deck.";
+    deck.shuffleDeck();
 
-    deck.sortDeck();
-    cout << "Sorted!" << endl;
-    deck.print();
-    //int index = deck.find(JackOfDiamons);
-    //if (index != -1)
-    //    cout << "Found the card at index no.:" << index << endl;
-    //else
-    //    cout << "not found the card!" << endl;
+    Deck newDeck = deck.mergeSort();
+    cout << "--- New deck ----- " << endl;
+    newDeck.print();
 
-    //cout << "my random number : " << myRandom (25, 52) << endl;
-    //cout << "new random number : " << newRandom (25, 52) << endl;
-    //cout << findBisect (JackOfDiamons, deck.cards, 0, 51) << endl;
-    //cout << "15 of Diamonds" << findBisect (FifteenOfDiamons, deck.cards, 0, 51) << endl;
     return 0;
-}  
+}
